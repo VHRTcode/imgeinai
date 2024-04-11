c2fc895e5e637690b9b416446adddf0d9fd8aa61
Vrushabh <166358744+VHRTcode@users.noreply.github.com>
2024-04-11 18:57:12 +0530

    Update TransformationForm.tsx
    changes transformations to transformation

--- components/shared/TransformationForm.tsx
+++ components/shared/TransformationForm.tsx
@@ -104,7 +104,7 @@ const TransformationForm = ({ action, data = null, userId, type, creditBalance, 
           if(newImage) {
             form.reset()
             setImage(data)
-            router.push(`/transformations/${newImage._id}`)
+            router.push(`/transformation/${newImage._id}`)
           }
         } catch (error) {
           console.log(error);
@@ -119,11 +119,11 @@ const TransformationForm = ({ action, data = null, userId, type, creditBalance, 
               _id: data._id
             },
             userId,
-            path: `/transformations/${data._id}`
+            path: `/transformation/${data._id}`
           })
 
           if(updatedImage) {
-            router.push(`/transformations/${updatedImage._id}`)
+            router.push(`/transformation/${updatedImage._id}`)
           }
         } catch (error) {
           console.log(error);
